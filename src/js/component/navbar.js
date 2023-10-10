48cd4a3955bbbc4e2e2dae96816446cdbfdcc9df
josefrometa <73325073+josefrometa@users.noreply.github.com>
2023-10-10 00:57:25 +0000

    comenzando

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,13 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3">
+		<nav className="navbar navbar-light bg-light mb-3 d-flex">
 			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand mb-0 ">
+					<img className=" mx-3" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/Star_Wars_Logo.svg/100px-Star_Wars_Logo.svg.png">
+
+					</img>
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
+					<button className="btn btn-primary mx-3">Favorites</button>
 				</Link>
 			</div>
 		</nav>
